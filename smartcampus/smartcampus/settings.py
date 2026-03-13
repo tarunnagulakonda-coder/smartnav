b9cb3d49a00995c1974b0783c99c74830b9d0f54
Deploy <deploy@smartcampus.local>
2026-03-13 09:20:38 +0530

    Fix Render build by using SQLite fallback for databases

--- smartcampus/smartcampus/settings.py
+++ smartcampus/smartcampus/settings.py
@@ -77,21 +77,15 @@ WSGI_APPLICATION = 'smartcampus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-if DATABASE_URL:
+if os.environ.get('DATABASE_URL'):
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL)
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'newdb',
-            'USER': 'postgres',
-            'PASSWORD': '1234',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
